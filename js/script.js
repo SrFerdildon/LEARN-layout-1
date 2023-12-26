39b6aa9e819a4b734c117ff9b1967da6d296d0eb
SrFerdildon <srferdildon@gmail.com>
2023-12-26 14:26:15 +0300

    created banner with slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-	console.log('proverka script.js');
+	const bannerSwiper = new Swiper('.banner-swiper', {
+		// Optional parameters
+		speed: 1000,
+		spaceBetween: 0,
+		direction: 'horizontal',
+		loop: true,
+
+		// If we need pagination
+		pagination: {
+			el: '.banner-swiper .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+
+		// Navigation arrows
+		navigation: {
+			nextEl: '.banner-swiper .swiper-button-next',
+			prevEl: '.banner-swiper .swiper-button-prev',
+		},
+	});
 })
