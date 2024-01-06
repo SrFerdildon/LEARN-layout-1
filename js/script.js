346b24ce365e4cda6113c45e7d5743b1752ee0d9
SrFerdildon <srferdildon@gmail.com>
2024-01-06 20:11:15 +0300

    created gallery block with animations

--- js/script.js
+++ js/script.js
@@ -34,4 +34,42 @@ document.addEventListener('DOMContentLoaded', () => {
 	$('.modal-form-close').on('click', function () {
 		$.magnificPopup.close();
 	});
+
+	$('.gallery-item').magnificPopup({
+		type: 'image',
+		showCloseBtn: false,
+		gallery: { enabled: true },
+		callbacks: {
+			beforeOpen: function () {
+				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+				this.st.mainClass = this.st.el.attr('data-effect');
+			}
+		},
+	});
+
+	$('.gallery-button a').on('click', function (e) {
+		e.preventDefault();
+		var galleryItem = $('.gallery-item');
+
+		if ($(this).hasClass('is-active')) {
+			$(this).removeClass('is-active');
+			$(this).text('Показать еще');
+			galleryItem.each(function () {
+				if ($(this).hasClass('is-active')) {
+					$(this).removeClass('is-active');
+					$(this).slideUp();
+				}
+			});
+		}
+		else {
+			$(this).addClass('is-active');
+			$(this).text('Скрыть');
+			galleryItem.each(function () {
+				if (!$(this).is(':visible')) {
+					$(this).addClass('is-active');
+					$(this).slideDown();
+				}
+			});
+		}
+	});
 })
